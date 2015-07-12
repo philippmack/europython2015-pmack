3b66d523874e14dc58de30b2aee2e93857251d49
philippmack <pmack@NB-096.phi-tps.local>
2015-07-12 17:49:41 +0200

    added new test

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -6,7 +6,13 @@ import pandas.util.testing as pdt
 import numpy.testing as npt
 import numpy as np
 
-def test_prediction():
+def test_prediction_simple():
     df = pd.DataFrame( { 'PRODUCT_ID' : 1 , 'DATE' : pd.date_range('1/1/2011', periods=5, freq='D'), 'SALES' : [2,3,4,5,6]})
     result = pr.simple_prediction(df,'PRODUCT_ID')
     npt.assert_array_equal(result['PREDICTION'], [np.nan,2,3,4,5])
+
+def test_prediction_rmean():
+    df = pd.DataFrame( { 'PRODUCT_ID' : 1 , 'DATE' : pd.date_range('1/1/2011', periods=6, freq='D'), 'SALES' : [2,1,2,1,4,1]})
+    result = pr.rmean_prediction(df,'PRODUCT_ID',5)
+    npt.assert_array_equal(result['PREDICTION'], [np.nan]*5 + [2.0])
+
