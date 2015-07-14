3a0a77d6b5a09f53056559ab42ab5c1c19b79cfa
Philipp Mack <philipp.mack@blue-yonder.com>
2015-07-14 09:47:39 +0200

    replenishment rules added

---
+++ replenishment/replenishment_rules.py
@@ -0,0 +1,28 @@
+from scipy.stats import poisson, skellam
+import pandas as pd
+import numpy as np
+import logging
+
+
+def stochastic_round(value) :
+    """
+        This rounds a float stochastically to an integer.
+    """
+    return int(round(value + np.random.uniform(-0.5, 0.5)))
+
+### simple rule
+def order_one_replenishment(stock,open_orders,quantile,sales_prog):
+    return 1.0
+
+### order expectation value
+def order_expectation_replenishment(stock,open_orders,quantile,sales_prog):
+    order = max(0,(sales_prog) - (stock + open_orders))
+    return stochastic_round(order)
+
+### general rule
+### order = predicted demand - ( expected stock + open_order)
+def replenishment(stock,open_orders,quantile,sales_prog):
+    sales_quant = poisson.ppf(quantile/100.,sales_prog+1.E-3)
+    order = max(0,(sales_quant) - (stock + open_orders))
+    return stochastic_round(order)
+
