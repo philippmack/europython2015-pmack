aeaa87ec7b5397612f5c5d12f6e40892cbe8ab92
Philipp Mack <philipp.mack@blue-yonder.com>
2015-07-14 11:19:54 +0200

    changes

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -13,28 +13,27 @@ def simulate_per_quantile(quantile):
    df = pd.read_csv('temp.csv',sep=';')
    df = df.sort(['PRODUCT_ID','DATE'])
    df = df.reset_index()
-   
 
    prediction = "rmean_prediction"
    prediction_func = eval(prediction)
-
    prediction_func(df,'PRODUCT_ID',5, 0)
 
    replenishment_rule="replenishment"
-
    replenishment_rule_func = eval(replenishment_rule)
 
    df['ORDER'] = df['PREDICTION'].apply(lambda x : replenishment_rule_func(0,0,quantile,x))
-
    df['INCOMING'] = df.groupby('PRODUCT_ID')['ORDER'].transform(lambda x :  x.shift(1) )
    df['STOCK_YESTERDAY'] = 0.0
   
    df_temp = pd.DataFrame( map(stock_simulation_rule,df['STOCK_YESTERDAY'], df['SALES'], df['INCOMING']), columns = ['STOCK', 'missing', 'surplus'])
+   
    df['SIM_LIB_MG']= df_temp['surplus']
    df['SIM_OVA_MG']= df_temp['missing']
 
    df = df.dropna()
    abschrift =  float(df.SIM_LIB_MG.sum())/df['SALES'].sum()
+
+   df.to_csv('temp_%s.csv' %quantile, sep=';')
 
    return abschrift,len(df[df['SIM_OVA_MG']>0])/float(len(df)),quantile
 
