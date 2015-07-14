b25214afe8cee48b962dfe37dcbb4c9b3fae56be
Philipp Mack <philipp.mack@blue-yonder.com>
2015-07-14 11:33:16 +0200

    testing replenishment rules

---
+++ tests/test_replenishment.py
@@ -0,0 +1,21 @@
+__author__ = 'pmack'
+
+import replenishment.replenishment_rules as rp
+import pandas as pd
+import pandas.util.testing as pdt
+import numpy.testing as npt
+import numpy as np
+
+
+def test_order_one_replenishment():
+    df = pd.DataFrame( { 'PRODUCT_ID' : 1 , 'DATE' : pd.date_range('1/1/2011', periods=5, freq='D'), 'PREDICTION' : [2,3,4,5,6]})
+    quantile = 10
+    df['ORDER'] = df['PREDICTION'].apply(lambda x : rp.order_one_replenishment(0,0,quantile,x))
+    npt.assert_array_equal(df['ORDER'], [1]*5)
+
+def test_order_expectation_replenishment():
+    df = pd.DataFrame( { 'PRODUCT_ID' : 1 , 'DATE' : pd.date_range('1/1/2011',periods=5, freq='D'), 'PREDICTION' : [2,3,4,5,6]})
+    quantile = 10
+    df['ORDER'] = df['PREDICTION'].apply(lambda x : rp.order_expectation_replenishment(0,0,quantile,x))
+    npt.assert_array_equal(df['ORDER'], [2,3,4,5,6])
+
