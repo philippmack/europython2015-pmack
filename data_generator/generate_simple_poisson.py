65b5855ff588f3b68927b85cef8fa821b3be1e05
philippmack <pmack@NB-096.phi-tps.local>
2015-07-22 15:55:44 +0200

    changed interface

--- data_generator/generate_simple_poisson.py
+++ data_generator/generate_simple_poisson.py
@@ -1,26 +1,47 @@
 __author__ = 'pmack'
 
+import argparse
 import pandas as pd
 import numpy as np
 from scipy.stats import poisson
 
-def generate_data():
-    product = np.arange(100)
-    date_rng = pd.date_range('2014-01-01','2015-06-01',freq='D')
-    product_rand = np.random.randint(1,10,100)
+
+def generate_data(number_of_products,start,end):
+    '''
+
+    :param number_of_products: number of items to generate
+    :param start: start date
+    :param end: end date
+    :return: dataframe with timeseries of poisson distributed sales per product
+    '''
+    product = np.arange(number_of_products)
+    date_rng = pd.date_range(start,end,freq='D')
+    product_rand_sales = np.random.randint(1,10,100)
 
     df = pd.DataFrame(columns=['PRODUCT_ID','DATE','SALES'])
 
     temp_df= []
 
+    '''
+    loop over products, not fast but for this simple example OK
+    '''
     for i,p in enumerate(product) :
-        sales_mean = product_rand[i]
+        sales_mean = product_rand_sales[i]
         sales = np.random.poisson(sales_mean,len(date_rng))
         temp_df.append(pd.DataFrame({'PRODUCT_ID' : i, 'DATE' : date_rng, 'SALES' : sales}))
-
 
     df = pd.concat(temp_df)
     return df
 
-dfr = generate_data()
-dfr.to_csv('temp.csv',sep=';')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o","--outputfile", help="outputfilename", type=str)
+    parser.add_argument("-n","--number", help="number_of_products", type=int)
+    parser.add_argument("-s","--startdate", help="start date", type=str)
+    parser.add_argument("-e","--enddate", help="end date", type=str)
+
+    args = parser.parse_args()
+
+    dfr = generate_data(args.number,args.startdate,args.enddate)
+    dfr.to_csv(args.outputfile,sep=';')
