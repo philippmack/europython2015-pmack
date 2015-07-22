1cec85f3758ff9a5edc0eecf1f3618ee88dc8f6c
philippmack <pmack@NB-096.phi-tps.local>
2015-07-22 16:00:14 +0200

    configurable

--- simulation.py
+++ simulation.py
@@ -3,20 +3,39 @@ import numpy as np
 from multiprocessing import Pool
 import simulation.simulation as sim
 import sys
-
+from config.yaml_parser import *
+from config.validator import *
+import itertools
+import argparse
+import time
 
 if __name__ == "__main__":
 
-   #read in config
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-y","--yaml", help="yaml inputfile to test", type=str)
+   parser.add_argument("-v","--version", help="version_id", type=str)
 
-   version_id =sys.argv[1]
-   liste = np.arange(10,100,10)
+   args = parser.parse_args()
+
+   # parse config file
+   sim_args = parse_yaml(args.yaml)
+
+   liste = sim_args['simulation']['quantiles']
+   infile = sim_args['simulation']['quantiles']
+
+   list_args = itertools.izip(liste, itertools.repeat(sim_args))
+
+   start_time = time.time()
 
    pool = Pool(processes=4)
-   result = pool.map(sim.simulate_per_quantile,liste)
+   result = pool.map(sim.simulate_wrapper,list_args)
+
+   end_time = time.time()
+
+   print "--- %s seconds ---" % (end_time - start_time)
    print result
 
    dfxy = pd.DataFrame( result, columns=['SURPLUS', 'OOS', 'QUANTILE'])
-   dfxy['VERSION_ID']=version_id
-   dfxy.to_csv('test_result%s.csv' %version_id,sep=';')
+   dfxy['VERSION_ID']=args.version
+   dfxy.to_csv('test_result%s.csv' %args.version,sep=';')
 
