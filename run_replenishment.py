63aa837b4bd4be9ffae3fac8a1a1386bb6b33c23
philippmack <pmack@NB-096.phi-tps.local>
2015-07-23 15:14:27 +0200

    updates

--- run_replenishment.py
+++ run_replenishment.py
@@ -10,23 +10,30 @@ import argparse
 import time
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-y", "--yaml", help="yaml inputfile to test", type=str)
 
-   parser = argparse.ArgumentParser()
-   parser.add_argument("-y","--yaml", help="yaml inputfile to test", type=str)
+    args = parser.parse_args()
 
-   args = parser.parse_args()
+    # parse config file
+    sim_args = parse_yaml(args.yaml)
+    test_file(sim_args, 'replenishment')
+    print sim_args
 
-   # parse config file
-   sim_args = parse_yaml(args.yaml)
+    start_time = time.time()
+    '''
+    read in file
+    '''
+    df = pd.read_csv('temp.csv',sep=';')
+    df = df[df['DATE']<='2015-03-01']
+    df = df.sort(['PRODUCT_ID','DATE'])
+    df = df.reset_index()
 
 
-   start_time = time.time()
+    order_df = rep.order(df,sim_args)
 
-   order_df = rep.order(sim_args)
+    end_time = time.time()
 
-   end_time = time.time()
+    print "--- %s seconds ---" % (end_time - start_time)
 
-   print "--- %s seconds ---" % (end_time - start_time)
-
-   order_df.to_csv('order.csv',sep=';')
-
+    order_df.to_csv('order.csv', sep=';')
