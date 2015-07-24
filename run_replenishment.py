6d11e7dea7d67da63e481b437faef428b66cf872
philippmack <pmack@NB-096.phi-tps.local>
2015-07-24 07:54:31 +0200

    update

--- run_replenishment.py
+++ run_replenishment.py
@@ -8,29 +8,30 @@ from config.validator import *
 import itertools
 import argparse
 import time
+import datetime
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-y", "--yaml", help="yaml inputfile to test", type=str)
-
+    parser.add_argument("-d", "--orderday", help="date of order", type=str)
     args = parser.parse_args()
 
     # parse config file
-    sim_args = parse_yaml(args.yaml)
-    test_file(sim_args, 'replenishment')
-    print sim_args
+    rep_args = parse_yaml(args.yaml)
+    test_file(rep_args, 'replenishment')
 
     start_time = time.time()
     '''
     read in file
     '''
-    df = pd.read_csv('temp.csv',sep=';')
-    df = df[df['DATE']<='2015-03-01']
+    df = pd.read_csv(rep_args['replenishment']['input_file'],sep=';', parse_dates=['DATE'])
+    orderday = datetime.datetime.strptime(args.orderday,'%Y-%m-%d')
+    df = df[df['DATE']<=orderday]
     df = df.sort(['PRODUCT_ID','DATE'])
     df = df.reset_index()
 
-
-    order_df = rep.order(df,sim_args)
+    order_df = rep.order(df,rep_args)
+    order_df = order_df[order_df['DATE']==orderday]
 
     end_time = time.time()
 
